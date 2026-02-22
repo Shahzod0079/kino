5e310b28e85a8429c0f95439c60b25a2faf65d68
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-02-22 09:52:52 +0500

    Создание классов Modell. Тошматов

---
+++ kino/Modell/Kinoteatr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kino.Modell
+{
+    public class Kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set; }
+        public int Count { get; set; }
+
+        public Kinoteatr(int Id, string Name, int CountZal, int Count)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+    }
+}
